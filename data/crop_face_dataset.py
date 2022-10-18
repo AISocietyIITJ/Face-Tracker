5705ac157be8a55e753fa639d2943956f0932162
Akhil-blanc <akhil0012893@gmail.com>
2022-10-18 07:34:26 +0530

    changed the sampling size to 10 files

--- data/crop_face_dataset.py
+++ data/crop_face_dataset.py
@@ -26,54 +26,60 @@ def ImportAndCrop():
         model_selection=0,
         min_detection_confidence=0.5
         ) as face_detection:
-            for root in dict:
-                class_num=os.path.split(root)[1]
-                os.mkdir(f'./Cropped_Face_Data/{class_num}')
+            for idx,root in enumerate(dict):
+                if idx<5:
+                    class_num=os.path.split(root)[1]
+                    os.mkdir(f'./Cropped_Face_Data/{class_num}')
 
-                #Detect all the faces in the image
-                for file in dict[root]:
-                    image=cv2.imread(os.path.join(root,file))
-                    h, w, _ = image.shape
-                    x_max = 0
-                    y_max = 0
-                    x_min = h
-                    y_min = w
-                    image.flags.writeable = True
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-                    results = face_detection.process(image)
-                    image.flags.writeable = True
-                    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-                    img_h, img_w, _ = image.shape
+                    #Detect all the faces in the image
+                    for idx1,file in enumerate(dict[root]):
+                        if idx1<2:
+                            image=cv2.imread(os.path.join(root,file))
+                            h, w, _ = image.shape
+                            x_max = 0
+                            y_max = 0
+                            x_min = h
+                            y_min = w
+                            image.flags.writeable = True
+                            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+                            results = face_detection.process(image)
+                            image.flags.writeable = True
+                            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                            img_h, img_w, _ = image.shape
 
-                    #choose the face which has largest width and full face is inside image
-                    if results.detections:
-                        max_width=0
-                        for detection in results.detections:
-                            x=detection.location_data.relative_bounding_box.xmin
-                            y=detection.location_data.relative_bounding_box.ymin
-                            height=detection.location_data.relative_bounding_box.height
-                            width= detection.location_data.relative_bounding_box.width
-                            if x>=0 and y>=0 and height>0 and  width>max_width:
-                                max_width=width
-                                max_one=detection
+                            #choose the face which has largest width and full face is inside image
+                            if results.detections:
+                                max_width=0
+                                for detection in results.detections:
+                                    x=detection.location_data.relative_bounding_box.xmin
+                                    y=detection.location_data.relative_bounding_box.ymin
+                                    height=detection.location_data.relative_bounding_box.height
+                                    width= detection.location_data.relative_bounding_box.width
+                                    if x>=0 and y>=0 and height>0 and  width>max_width:
+                                        max_width=width
+                                        max_one=detection
 
-                        #crop the image
-                        x_min=min(x_min,max_one.location_data.relative_bounding_box.xmin)
-                        y_min=min(y_min,max_one.location_data.relative_bounding_box.ymin)
-                        height=max(x_max,max_one.location_data.relative_bounding_box.height)
-                        width=max(y_max,max_one.location_data.relative_bounding_box.width)
-                        x_1 = int((x_min)*img_w)
-                        x_2 = int((x_min + width)*img_w)
-                        y_1 = int((y_min)*img_h)
-                        y_2 = int((y_min + height)*img_h)
-                    roi = image[y_1:y_2, x_1:x_2]
+                                #crop the image
+                                x_min=min(x_min,max_one.location_data.relative_bounding_box.xmin)
+                                y_min=min(y_min,max_one.location_data.relative_bounding_box.ymin)
+                                height=max(x_max,max_one.location_data.relative_bounding_box.height)
+                                width=max(y_max,max_one.location_data.relative_bounding_box.width)
+                                x_1 = int((x_min)*img_w)
+                                x_2 = int((x_min + width)*img_w)
+                                y_1 = int((y_min)*img_h)
+                                y_2 = int((y_min + height)*img_h)
+                            roi = image[y_1:y_2, x_1:x_2]
 
-                    #save the image
-                    try:
-                        cv2.imwrite(f'.//Cropped_Face_Data/{class_num}/{file}',img = roi )
-                    except:
-                        print(class_num, file)
-                        continue
+                            #save the image
+                            try:
+                                cv2.imwrite(f'.//Cropped_Face_Data/{class_num}/{file}',img = roi )
+                            except:
+                                print(class_num, file)
+                                continue
+                        else:
+                            break
+                else:
+                    break
     except:
         shutil.rmtree('./Cropped_Face_Data')
 
